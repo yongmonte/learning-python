8da1e861eae3467f1042f57882c52e0fb234b5a7
Joe Marini <cjmarini68@gmail.com>
2025-01-15 23:09:10 +0000

    updated start point

--- Start/Ch3/modules_start.py
+++ Start/Ch3/modules_start.py
@@ -2,11 +2,18 @@
 # Working with modules of code
 
 # import the math module, which contains features for working with mathematics
+import math
+
+# print("The square root of 16 is", math.sqrt(16))
+# print("Pi is", math.pi)
 
 # import a specific part of the module so you can refer to it more easily
+from math import pi
+# print("Pi is", pi)
 
 # import a module and give it a different name
-
+import random as r
+print(r.randint(100,200))
 
 # the math module contains lots of pre-built functions
 
@@ -18,3 +25,15 @@
 
 
 # try some of the math functions for yourself here:
+
+# Use the 3rd party tabulate module to print tabulated data:
+
+# Sample data
+data = [
+  ["Product", "Price", "Stock"],
+  ["Laptop", 999.99, 45],
+  ["Mouse", 24.99, 128],
+  ["Keyboard", 59.99, 89]
+]
+
+# Create a formatted table
