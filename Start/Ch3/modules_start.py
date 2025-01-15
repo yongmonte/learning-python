ae4785faa817436283d680debde58d9e0cb24c1c
Joe Marini <cjmarini68@gmail.com>
2025-01-15 23:10:17 +0000

    updated start

--- Start/Ch3/modules_start.py
+++ Start/Ch3/modules_start.py
@@ -2,18 +2,13 @@
 # Working with modules of code
 
 # import the math module, which contains features for working with mathematics
-import math
 
-# print("The square root of 16 is", math.sqrt(16))
-# print("Pi is", math.pi)
 
 # import a specific part of the module so you can refer to it more easily
-from math import pi
-# print("Pi is", pi)
+
 
 # import a module and give it a different name
-import random as r
-print(r.randint(100,200))
+
 
 # the math module contains lots of pre-built functions
 
