068a3acda424551faab4f0264d2a0c8d148673f1
Joe Marini <cjmarini68@gmail.com>
2025-01-16 18:21:31 +0000

    Change finished state

--- Finished/Ch6/writefile_finished.py
+++ Finished/Ch6/writefile_finished.py
@@ -5,12 +5,14 @@
 
 # Open a file for writing and create it if it doesn't exist
 sample_file = open("textfile.txt","w+")
+sample_file.write("This is some sample text in our sample file.")
+sample_file.close()
 
 # Open the file for appending text to the end
 sample_file = open("textfile.txt","a+")
 
 # write some lines of data to the file
-sample_file.write("This is some sample text in our sample file.")
+sample_file.write("This is more sample text in our sample file.")
 
 # close the file when done
 sample_file.close()
