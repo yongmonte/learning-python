713bd4f9d63873505eeea094db402c966c5bafea
joemarini <cjmarini68@gmail.com>
2025-01-15 22:24:26 -0800

    Sequences and dictionaries

---
+++ Start/Ch2/sequence_start.py
@@ -0,0 +1,28 @@
+# LinkedIn Learning Python course by Joe Marini
+# Example file for complex types
+
+# Sequences: Lists and Tuples
+# These are -- surprise -- sequences of values
+
+
+# to access a member of a sequence type, use []
+
+
+# add a list to another list
+
+
+# use slices to get parts of a sequence
+
+
+# you can use slices to reverse a sequence
+
+
+# Tuples are like lists, but they are immutable
+
+
+# Sets are also sequences, but they contain unique values
+
+# Set, however, can not be indexed like lists or tuples
+# print(myset[0]) # this will cause an error
+
+# Test for membership
